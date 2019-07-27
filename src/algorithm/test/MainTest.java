b15850edce028fd33e80dd4a65fdee0096d2a89d
zomggang <gangeun25@gmail.com>
2019-07-27 20:15:40 +0900

    test

--- src/algorithm/test/MainTest.java
+++ src/algorithm/test/MainTest.java
@@ -4,11 +4,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import algorithm.continiousArray1.ContiniousArray01;
+
 class MainTest {
 
 	@Test
 	void test() {
-		
+		ContiniousArray01 continiousArray01 = new ContiniousArray01();
+		String[] args = {"1","4","3","2"};
+		continiousArray01.main(args);
 	}
 
 }
